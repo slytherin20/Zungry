286f1decb8cf18f082225d2c1580789a63abf78d
slytherin20 <sonaligupta2904@gmail.com>
2023-10-30 18:12:07 +0530

    STYLE- Replace item popup styling

--- src/Components/ReplaceItemsPopup.js
+++ src/Components/ReplaceItemsPopup.js
@@ -51,18 +51,24 @@ export function ReplaceItemsPopup({
     }
   }
   return (
-    <article className="w-1/2 h-32 bg-white p-3">
+    <article className="w-1/2  bg-white p-4 rounded-md shadow-md">
       <h2 className="font-bold">Replace cart items?</h2>
-      <p>
+      <p className="text-sm">
         Your cart contains dishes from {from}. Do you wish to discard the
         selection and add dishes from {to}?
       </p>
 
-      <section className="w-3/4 flex justify-between m-auto">
-        <button className="w-28 bg-red-500" onClick={toggleHandler}>
+      <section className="w-full flex justify-evenly m-auto">
+        <button
+          className="w-20 border border-red-700 rounded-md text-red-700 hover:bg-slate-200"
+          onClick={toggleHandler}
+        >
           No
         </button>
-        <button className="w-28 bg-red-500" onClick={replaceCartItems}>
+        <button
+          className="w-20 bg-red-700 text-white rounded-md hover:bg-red-500"
+          onClick={replaceCartItems}
+        >
           Replace
         </button>
       </section>
